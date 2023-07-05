489c73b32e7bcf28963beaa560e9e4b5670944fb
Zachary King <1266571+zchrykng@users.noreply.github.com>
2023-07-05 14:01:34 -0400

    add basic function

---
+++ zsh-mvlink.plugin.zsh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+function mvlink() {
+    original="$1" target="$2"
+
+    if [ -d "$target" ]; then
+      target="$target/${original##*/}"
+    fi
+    mv -- "$original" "$target"
+    case "$original" in
+      */*)
+        case "$target" in
+          /*) :;;
+          *) target="$(cd -- "$(dirname -- "$target")" && pwd)/${target##*/}"
+        esac
+    esac
+    ln -s -- "$target" "$original"
+}
+
